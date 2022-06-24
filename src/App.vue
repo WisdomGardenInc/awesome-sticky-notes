30a551f7926a83f3be27b3ef6478e8c5f5374fb9
chen <kikyous@163.com>
2022-06-24 14:45:55 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import VueDragResize from 'vue-drag-resize'
+</script>
+
+<template>
+  <VueDragResize :isActive="true" dragHandle=".drag">
+    <button class="drag">title</button>
+    <div>test</div>
+  </VueDragResize>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
