d290cb12aebe30fa389bc983ac13b83866f8cafb
chen <kikyous@163.com>
2022-07-08 15:43:39 +0800

    save note content and position

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useLocalStorage } from '@vueuse/core'
 import VueDragResize from 'vue-drag-resize-2'
+
+interface Position {
+  left: Number, //the X position of the component
+  top: Number, //the Y position of the component
+  width: Number, //the width of the component
+  height: Number //the height of the component
+}
+
+class Note {
+  public content = ''
+  public ts = 0;
+  public position: Position = { left: 50, top: 50, width: 100, height: 100 }
+  constructor() {
+    this.ts = new Date().getTime()
+  }
+}
+
+const notes = ref<Note[]>([new Note()])
+
+const newNote = () => {
+  notes.value.push(new Note())
+}
+
+const onDragstop = (position: Position, note: any) => {
+  note.position = position
+}
+
+useLocalStorage('notes', notes)
+
 </script>
 
 <template>
-  <VueDragResize  dragHandle=".drag">
-    <div class="drag w-full h-4 bg-light-800" ></div>
-    <textarea name="" id="" class="w-full h-full"></textarea>
+  <button @click="newNote">+</button>
+  <VueDragResize v-for="note in notes" :key="note.ts" :x="note.position.left" :y="note.position.top"
+    @dragstop="onDragstop($event, note)" dragHandle=".drag" class="bg-yellow-200">
+    <div class="drag w-full h-4 bg-light-800"></div>
+    <textarea class="w-full h-full" v-model="note.content"></textarea>
   </VueDragResize>
 </template>
 
@@ -19,11 +52,15 @@ import VueDragResize from 'vue-drag-resize-2'
   margin-top: 60px;
 }
 
+textarea {
+  box-sizing: border-box;
+  border: none;
+  background: transparent;
+  resize: none;
+}
+
 :deep(.content-container) {
   display: flex;
   flex-direction: column;
-}
-:deep(.content-box) {
-  box-sizing: border-box;
 }
 </style>
