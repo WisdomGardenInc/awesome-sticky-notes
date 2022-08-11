489b480d3414fe4b669c7f54cd8ea2aab6e39414
chen <kikyous@163.com>
2022-08-11 18:25:33 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,18 @@ import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 import presetIcons from '@unocss/preset-icons'
 import presetWind from '@unocss/preset-wind'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Unocss({
-    presets: [
-      presetIcons(),
-      presetWind()
-    ]
-  })]
+  plugins: [
+    vue(),
+    Unocss({
+      presets: [
+        presetIcons(),
+        presetWind()
+      ]
+    }),
+    VitePWA({ registerType: 'autoUpdate' })
+  ]
 })
